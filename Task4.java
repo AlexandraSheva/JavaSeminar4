a60f7b34e5824fbdae585238a7a381a02e11a4fd
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-07-04 16:29:16 +0300

    task4

---
+++ Task4.java
@@ -0,0 +1,30 @@
+public class Task4 {
+  
+    /*Реализовать стэк с помощью массива. 
+Нужно реализовать методы:
+size(), empty(), push(), peek(), pop(). 
+ */
+  
+public static void main(String[] args) {
+  stack();
+}
+    
+    static void stack() {
+    MyStack myStack = new MyStack();
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.push(5);
+    System.out.println(myStack.empty());
+    System.out.println(myStack.pop());
+    System.out.println(myStack.peek());
+    System.out.println(myStack.size());
+
+
+
+
+
+  }
+
+}
